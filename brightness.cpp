4b675a68052e526686677781df60107d9755461e
deall <119384746+d8425@users.noreply.github.com>
2026-05-22 09:10:25 +0800

    Add files via upload

--- brightness.cpp
+++ brightness.cpp
@@ -1,22 +1,32 @@
-#include "brightness.h"
+﻿#include "brightness.h"
+#include "get_brightness_cuda.cuh"
+#include "cuda_runtime.h"
+#include "tools.h"
 
 cv::Mat _get_kernel(double mapping) {
-	int mid_mapping = int(mapping);
+    int mid_mapping = int(mapping);
+    double res = mapping - mid_mapping;
 
-	cv::Mat kernel;
-	kernel.create(mid_mapping + 2, mid_mapping + 2, CV_32F);
+    //if mid_mapping is even
+    if (mid_mapping % 2 == 0) {
+        //mid_mapping += 1;
+        mid_mapping -= 1;
+    }
 
-	kernel = (mapping - mid_mapping)/2; // �ⲿ����ӦΪһ��
-	kernel(cv::Range(1, mid_mapping + 1), cv::Range(1, mid_mapping + 1)) = 1;
+    cv::Mat kernel;
+    kernel.create(mid_mapping + 2, mid_mapping + 2, CV_32F);
+
+    kernel = res / 2; // Íâ²¿ÄÜÁ¿Ó¦ÎªÒ»°ë
+    kernel(cv::Range(1, mid_mapping + 1), cv::Range(1, mid_mapping + 1)) = 1;
     cv::Scalar kernel_sum = cv::sum(kernel);
-	kernel = kernel * (1/kernel_sum[0]);
-	return kernel;
+    kernel = kernel * (1 / kernel_sum[0]);
+    return kernel;
 }
 
 void _get_brightness(cv::Mat img, cv::Mat mapx, cv::Mat mapy,
     cv::Mat kernel, cv::Mat& csv)
 {
-    // ��֤���� & ����
+    // ±£Ö¤Á¬Ðø & ÀàÐÍ
     img = img.clone();
     mapx = mapx.clone();
     mapy = mapy.clone();
@@ -39,7 +49,7 @@ void _get_brightness(cv::Mat img, cv::Mat mapx, cv::Mat mapy,
             int x = static_cast<int>(mx[j]);
             int y = static_cast<int>(my[j]);
 
-            if (x < rad || y < rad || x >= img.rows - rad || y >= img.cols - rad) {
+            if (x < rad || y < rad || x >= img.rows - rad || y >= img.cols - rad) { // È¥³ý±ßÔµµã(mapÖÐµÄ¼«Ð¡Öµµã»ò¸ºÊý»á±»ºöÂÔ)
                 out[j] = 0.0f;
                 continue;
             }
@@ -50,20 +60,96 @@ void _get_brightness(cv::Mat img, cv::Mat mapx, cv::Mat mapy,
                 for (int dx = 0; dx < ksz; ++dx)
                     sum += base[dy * img.cols + dx] * k[dy * ksz + dx];
             out[j] = sum;
+
         }
     }
 }
 
-cv::Mat get_brt(const cv::Mat& img, cv::Mat& mapx, cv::Mat& mapy, double mapping,double panel_rows,double panel_cols) {
-	//get brightness kernel
-	cv::Mat kernel;
-	kernel = _get_kernel(mapping);
+void _get_brightness_cuda(cv::Mat img, cv::Mat mapx, cv::Mat mapy,
+    cv::Mat kernel, cv::Mat& csv)
+{
+    // ±£Ö¤Á¬Ðø & ÀàÐÍ
+    img = img.clone();
+    mapx = mapx.clone();
+    mapy = mapy.clone();
+    CV_Assert(img.type() == CV_32FC1);
+    CV_Assert(mapx.type() == CV_32FC1 && mapy.type() == CV_32FC1);
+    CV_Assert(kernel.type() == CV_32FC1 && kernel.rows == kernel.cols);
 
-	//get brightness
-	cv::Mat imgf, csv;
-	csv.create(panel_cols, panel_rows, CV_32F);
+    csv.create(mapx.size(), CV_32FC1);
+
+    const int rad = kernel.rows / 2;
+    const int ksz = kernel.rows;
+    const float* k = kernel.ptr<float>();
+
+    for (int i = 0; i < mapx.rows; ++i) {
+        const float* mx = mapx.ptr<float>(i);
+        const float* my = mapy.ptr<float>(i);
+        float* out = csv.ptr<float>(i);
+
+        for (int j = 0; j < mapx.cols; ++j) {
+            int x = static_cast<int>(mx[j]);
+            int y = static_cast<int>(my[j]);
+
+            if (x < rad || y < rad || x >= img.rows - rad || y >= img.cols - rad) { // È¥³ý±ßÔµµã(mapÖÐµÄ¼«Ð¡Öµµã»ò¸ºÊý»á±»ºöÂÔ)
+                out[j] = 0.0f;
+                continue;
+            }
+
+            const float* base = img.ptr<float>(x - rad) + (y - rad);
+            float sum = 0.0f;
+            for (int dy = 0; dy < ksz; ++dy)
+                for (int dx = 0; dx < ksz; ++dx)
+                    sum += base[dy * img.cols + dx] * k[dy * ksz + dx];
+            out[j] = sum;
+
+        }
+    }
+}
+
+cv::Mat get_brt(const cv::Mat& img, cv::Mat& mapx, cv::Mat& mapy, double mapping, double panel_rows, double panel_cols, std::array<int, 1> brightness_setting) {
+    //check cuda is available
+    int cuda_device_count = 0;
+    cudaError_t err = cudaGetDeviceCount(&cuda_device_count);
+
+    //get brightness kernel
+    cv::Mat kernel;
+    if (brightness_setting[0] != 0) {
+        mapping = brightness_setting[0];
+    }
+
+    kernel = _get_kernel(mapping);
+
+    //get brightness
+    cv::Mat imgf, csv;
+    csv.create(panel_cols, panel_rows, CV_32F);
 
     img.convertTo(imgf, CV_32FC1, 1.0 / 255.0);
-	_get_brightness(imgf, mapy, mapx, kernel, csv);
+
+    //main
+    csv.create(mapy.rows, mapy.cols, CV_32FC1);
+
+
+    if (cuda_device_count) {
+        //确保连续内存（CUDA 需要）
+        std::cout << get_time() << ":CUDA" << std::endl;
+        if (!imgf.isContinuous())  imgf = imgf.clone();
+        if (!mapy.isContinuous())  mapy = mapy.clone();
+        if (!mapx.isContinuous())  mapx = mapx.clone();
+        if (!kernel.isContinuous()) kernel = kernel.clone();
+
+        get_brightness_cuda_wrapper(
+            imgf.ptr<float>(), imgf.rows, imgf.cols,
+            mapy.ptr<float>(),
+            mapx.ptr<float>(),
+            kernel.ptr<float>(), kernel.rows,
+            csv.ptr<float>(), csv.rows, csv.cols
+        );
+    }
+    else {
+        std::cout << get_time() << ":CPU" << std::endl;
+        _get_brightness(imgf, mapy, mapx, kernel, csv);
+    }
+    
     return csv;
 }
